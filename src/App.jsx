deaf63cb6d3e152cfc0668dd16247dff416e6b71
tariq7299 <tr.sar77an78@gmail.com>
2024-05-12 19:32:06 +0300

    Created pages folder and created Components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from "./pages/Home/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Test text</h1>
+      <h1>Test text</h1>
+      <Home></Home>
     </>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../../components/NavBar/NavBar"
+
+function Home() {
+
+    return (
+
+        <>
+            <h1>Home page test</h1>
+            <NavBar></NavBar>
+        </>
+
+    )
+}
+
+export default Home
