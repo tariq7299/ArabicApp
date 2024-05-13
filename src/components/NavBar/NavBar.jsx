aa8ad001b779a1fbe2164ddf704fafb1aa0eb957
tariq7299 <tr.sar77an78@gmail.com>
2024-05-13 10:38:46 +0300

    Created the navbar and applied some basic styles to it also i made it good on all screen devices

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,32 +10,37 @@ import './NavBar.scss'
 function NavBar() {
   return (
     <>
-       <Navbar expand="lg" className="bg-white mb-3">
-  <Container fluid>
-  {/* <img src='/src/assets/navBarLogo.png' width='100' height='100'></img> */}
-    <Navbar.Brand href="#home">LOGO</Navbar.Brand>
-    <Navbar.Toggle className='navbar-toggler'  aria-controls={`offcanvasNavbar-expand-lg`} />
-    <Navbar.Offcanvas
-      id={`offcanvasNavbar-expand-lg`}
-      aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-      placement="end"
-    >
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-          Offcanvas
-        </Offcanvas.Title>
-      </Offcanvas.Header>
-      <Offcanvas.Body>
-        <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
+         <Navbar expand="lg" className="bg-white mb-3" fixed="top">
+           <Container fluid>
+           {/* <img src='/src/assets/navBarLogo.png' width='100' height='100'></img> */}
+             <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+             <Navbar.Toggle className='navbar-toggler'  aria-controls={`offcanvasNavbar-expand-lg`} />
+             <Navbar.Offcanvas
+               id={`offcanvasNavbar-expand-lg`}
+               aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+               placement="end"
+               className="expanded-navbar-body"
+             >
+               <Offcanvas.Header closeButton>
+          <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+            LOGO
+          </Offcanvas.Title>
+               </Offcanvas.Header>
+               <Offcanvas.Body >
+          <Nav className="justify-content-end flex-grow-1 pe-3 ">
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">About us</Nav.Link>
+            <Nav.Link href="#action2">Blog</Nav.Link>
+            <Nav.Link href="#action2">Courses</Nav.Link>
+            <Nav.Link href="#action2">Resources</Nav.Link>
+            <Nav.Link href="#action2">Contact us</Nav.Link>
          
-        </Nav>
-
-      </Offcanvas.Body>
-    </Navbar.Offcanvas>
-  </Container>
-</Navbar>
+          </Nav>
+         
+               </Offcanvas.Body>
+             </Navbar.Offcanvas>
+           </Container>
+         </Navbar>
     </>
   );
 }
--- to-do.md
+++ to-do.md
@@ -6,4 +6,5 @@
 
 ## NavBar  
 - Crop logo only form the logo image nested inside the navbar
+- Change the burger toggle icon to be black and bolder (You can't style it more you need to change the icon compleetely)
 - 
