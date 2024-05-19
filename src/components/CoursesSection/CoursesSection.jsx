670968d4d7e8a5af24d0219b66f5aef5d0410ca2
tariq7299 <tr.sar77an78@gmail.com>
2024-05-19 20:12:04 +0300

    - Created the course chain part - Creaed the basic structure of the courses section

--- src/components/CoursesSection/CoursesSection.jsx
+++ src/components/CoursesSection/CoursesSection.jsx
@@ -1,14 +1,24 @@
 import "./CoursesSection.scss"
 import MyTitle from "../common/MyTitle/MyTitle"
 import SectionMeta from "../common/SectionMeta/SectionMeta"
-import MyButton from "../MyButton/MyButton"
+import MyButton from "../common/MyButton/MyButton"
+import CourseCard from "../common/CourseCard/CourseCard"
+
 
 function CoursesSection() {
 
     return (
 
         <div className="Courses-section-container">
-            {/* Create the courses cards */}
+            <div className="courses-cards-wrapper">
+                <CourseCard></CourseCard>
+                <CourseCard></CourseCard>
+                <CourseCard></CourseCard>
+                <CourseCard></CourseCard>
+                <CourseCard></CourseCard>
+                <CourseCard></CourseCard>
+            </div>
+
             <MyTitle text="Courses for every skill level and interest"></MyTitle>  
             <SectionMeta>Focus on your specific goals with our guided courses, including academic test prep, business English, conversation practice, and more.</SectionMeta>
             <MyButton text="Explore all courses -->" buttonColor="secondary-color" textColor="primary-font-color"></MyButton>
--- src/components/common/CourseCard/CourseCard.jsx
+++ src/components/common/CourseCard/CourseCard.jsx
@@ -1,28 +1,21 @@
-import MyButton from '../common/MyButton/MyButton'
-import MyTitle from '../common/MyTitle/MyTitle'
-import SectionMeta from '../common/SectionMeta/SectionMeta'
-import './FreeResourcesSection.scss'
+import './CourseCard.scss'
 
-function FreeResourcesSection() {
+function CourseCard() {
 
     return (
-        <div className='free-resoureces-section-container section-gap'>
-            <div className='free-resources-elements-wrapper'>
+        <div className='course-card-wrapper'>  
 
-                <div className='title-button-meta-wrapper'>
-                    <h3>Try our free resources</h3> 
-                    <div className='section-meta-wrapper'>
-                    <SectionMeta>Get a taste of Arabic App with our most popular tools and guides. No subscription required.</SectionMeta>
-                    </div>
-                    <MyButton text="Join now" buttonColor="background-color-black" textColor="secondary-font-color"></MyButton>
-
-                </div>
-
-                <img src="https://staticimagescdn.cambly.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftry_cambly.3d7c8d26.png&w=640&q=75" alt="Cambly Resources"></img>
-
+            <div className='image-wrapper'>
+                <img src="https://staticimagescdn.cambly.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5e2b9a72db0da5490226b6b5.f6827192.png&amp;w=384&amp;q=75"></img>  
             </div>
+            <div className='course-details-wrapper'>
+                <h4>IELTS Speaking Part 1</h4>
+                <p>ANY LEVEL</p>
+                <p>8 LESSONS</p>
+            </div>
+
         </div>
     )
 }
 
-export default FreeResourcesSection
+export default CourseCard
