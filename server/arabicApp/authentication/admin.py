db0b44a555279c29b9f7b05e5c46a64d46dbb497
tariq7299 <tr.sar77an78@gmail.com>
2024-06-09 23:21:22 +0300

    - Extended the base user model of django to using proxy method - but i will change proxy method to be oneToOne method

--- server/arabicApp/authentication/admin.py
+++ server/arabicApp/authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tutor
 
-# Register your models here.
+
+class TutorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Tutor, TutorAdmin)
