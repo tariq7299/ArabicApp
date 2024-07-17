06032f8d7dcc0fd66207ce9e9e1fd777aa2c711e
tariq7299 <tr.sar77an78@gmail.com>
2024-07-17 22:04:12 +0300

    - Added crsrf token escept in urls.py to paypass csrf token

--- server/arabicApp/arabicApp/settings.py
+++ server/arabicApp/arabicApp/settings.py
@@ -74,7 +74,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'graphql_jwt.middleware.JSONWebTokenMiddleware',
 ]
 
 ROOT_URLCONF = 'arabicApp.urls'
