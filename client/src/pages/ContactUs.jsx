091aeae4145e5ee5f1bec7ee4b43580e0f0be0ec
Tariq Sarhan <tr.sar77an78@gmail.com>
2024-08-30 20:02:26 +0300

    - Added client side validation in contact us page

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -18,6 +18,7 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import FormHelperText from '@mui/material/FormHelperText';
 import MyButton from "../components/common/MyButton";
 import InputLabel from '@mui/material/InputLabel';
 import { useForm, Controller } from 'react-hook-form';
@@ -40,57 +41,9 @@ export default function ContactUs() {
     control
   } = useForm()
 
-  //   function returnOnlyTouchedFieldsVals(fieldsVals, touchedFields) {
-  //     const touchedFieldsss = Object.keys(fieldsVals).reduce((acc, key) => {
-  //         if (touchedFields[key]) {
-  //             acc[key] = fieldsVals[key];
-  //         }
-  //         return acc;
-  //     }, {});
-  //     console.log()
-  //     return touchedFieldsss
-  // }
-
-  function areAllFieldsEmpty(data) {
-    const areAllFiledsareEmpty = Object.values(data).every((value) => value === '' || value === undefined || value === null);
-    console.log("areAllFiledsareEmpty", areAllFiledsareEmpty)
-    return areAllFiledsareEmpty
-  }
-
-  function isSubmitButtonDisabled() {
-    // const formValues = getValues();
-    console.log("dirtyFields", dirtyFields)
-    console.log("areAllFieldsEmpty(dirtyFields", areAllFieldsEmpty(dirtyFields))
-    console.log("isValid", isValid)
-    console.log("isDirty", isDirty)
-    // const changedFields = this.returnOnlyTouchedFieldsVals(formValues, dirtyFields);
-    const value = !isDirty || areAllFieldsEmpty(dirtyFields);
-    // const value = !isDirty || !isValid || areAllFieldsEmpty(dirtyFields);
-    console.log("value", value)
-    return value;
-  }
 
 
-  // function isSubmitButtonDisabled() {
-  //   // If all fields is valid and contains content
-  //   // If fields got dirty
-  //   const watchFields = watch()
-  //   console.log("dirtyFields", dirtyFields)
-  //   const isAnyFieldFilled = Object.values(watchFields).some(
-  //     field => field?.fieldValue && (typeof field.fieldValue === 'string' && field.fieldValue?.trim()) !== ""
-  //   );
-  //   return isAnyFieldFilled
-  // }
-
-  // useEffect(() => {
-  //   const isAnyFieldFilled = Object.values(watchFields).some(
-  //     field => field?.fieldValue && (typeof field.fieldValue === 'string' && field.fieldValue?.trim()) !== ""
-  //   );
-  //   setIsSubmitEnabled(isAnyFieldFilled);
-  // }, [watchFields]);
-
-
-  const { loading: loadingChoices, error: errorFetchingChoices, data: selectFieldsChoices } = useQuery(GET_SELECT_FIELDS_CHOICES);
+  const { loading: loadingChoices, data: selectFieldsChoices } = useQuery(GET_SELECT_FIELDS_CHOICES);
 
   const [submitContactSubmission, { loading: submitting }] = useMutation(CREATE_CONTACT_SUBMISSION);
 
@@ -209,13 +162,19 @@ export default function ContactUs() {
                       name="firstName"
                       control={control}
                       defaultValue=""
-                      rules={{ required: true }}
-                      render={({ field }) => (
+                      rules={{ required: { value: true, message: "This field is required!" } }}
+                      render={({ field, fieldState: { error } }) => (
                         <TextField
-                          type="text"
                           {...field}
-                          label="First Name" variant="standard" placeholder="Ahmed" fullWidth
+                          error={!!error}
+                          helperText={error?.message}
+                          type="text"
+                          label="First Name"
+                          variant="standard"
+                          placeholder="Ahmed"
+                          fullWidth
                         />
+
                       )}
                     />
 
@@ -228,9 +187,12 @@ export default function ContactUs() {
                       name="lastName"
                       control={control}
                       defaultValue=""
-                      rules={{ required: true }}
-                      render={({ field }) => (
-                        <TextField type="text" {...field} label="Last Name" variant="standard" placeholder="Mostafa..." fullWidth />
+                      rules={{ required: { value: true, message: "This field is required!" } }}
+                      render={({ field, fieldState: { error } }) => (
+                        <TextField
+                          {...field}
+                          error={!!error}
+                          helperText={error?.message} label="Last Name" variant="standard" placeholder="Mostafa..." fullWidth />
                       )}
                     />
                   </div>
@@ -240,9 +202,12 @@ export default function ContactUs() {
                       name="email"
                       control={control}
                       defaultValue=""
-                      rules={{ required: true }}
-                      render={({ field }) => (
-                        <TextField {...field} type="email" label="Email" variant="standard" placeholder="tr.sar77an78@gmail.com.." fullWidth />
+                      rules={{ required: { value: true, message: "This field is required!" } }}
+                      render={({ field, fieldState: { error } }) => (
+                        <TextField
+                          {...field}
+                          error={!!error}
+                          helperText={error?.message} type="email" label="Email" variant="standard" placeholder="tr.sar77an78@gmail.com.." fullWidth />
                       )}
                     />
                   </div>
@@ -252,9 +217,18 @@ export default function ContactUs() {
                       name="phone"
                       control={control}
                       defaultValue=""
-                      rules={{ required: true }}
-                      render={({ field }) => (
-                        <TextField {...field} type="text" label="Phone Number" variant="standard" placeholder="01099133377.." fullWidth />
+                      rules={{
+                        required: { value: true, message: "This field is required!" }, pattern: {
+                          value: /^\+?1?\d{9,15}$/,
+                          message: "+999999999'. Up to 15 digits allowed in phone."
+                        }
+                      }}
+
+                      render={({ field, fieldState: { error } }) => (
+                        <TextField
+                          {...field}
+                          error={!!error}
+                          helperText={error?.message} type="text" label="Phone Number" variant="standard" placeholder="01099133377.." fullWidth />
                       )}
                     />
                   </div>
@@ -265,7 +239,7 @@ export default function ContactUs() {
                       control={control}
                       defaultValue=""
                       rules={{
-                        required: true,
+                        required: { value: true, message: "This field is required!" },
                         valueAsNumber: true,
                         min: {
                           value: 7,
@@ -276,84 +250,98 @@ export default function ContactUs() {
                           message: "Age must be less than 120."
                         }
                       }}
-                      render={({ field }) => (
-
-                        <TextField {...field} onChange={(e) => field.onChange(parseInt(e.target.value, 10) || "")}
+                      render={({ field, fieldState: { error } }) => (
+                        <TextField
+                          {...field}
+                          error={!!error}
+                          helperText={error?.message} onChange={(e) => field.onChange(parseInt(e.target.value, 10) || "")}
                           onSubmit={(e) => field.onSubmit(parseInt(e.target.value, 10) || 0)}
                           type="text" label="Age" variant="standard" placeholder="24.." fullWidth />
                       )}
                     />
                   </div>
-                  {/* <h1>{errors?.age?.message}</h1> */}
 
-                  <div className="col-12 col-lg-4 py-4 pe-xl-4 max-400 mx-auto mx-lg-0 ">
-
-                    <InputLabel htmlFor="nativeLanguage" id="nativeLanguage-label">Native Language</InputLabel>
+                  <div className="col-12 col-lg-4 py-5 pe-xl-4 max-400 mx-auto mx-lg-0">
                     <Controller
                       name="nativeLanguage"
                       control={control}
-                      defaultValue={languageChoices[0][0]}
-                      rules={{ required: true }}
-                      render={({ field }) => (
-                        <Select
-                          {...field}
-                          id="nativeLanguage"
-                          aria-labelledby="nativeLanguage-label"
-                          fullWidth
-                          variant="standard"
-                        >
-                          {languageChoices?.map(([value, label]) => (
-                            <MenuItem key={value} value={value}>{label}</MenuItem>
-                          ))}
-                        </Select>
+                      // defaultValue={languageChoices[0][0]}
+                      defaultValue=""
+                      rules={{ required: { value: true, message: "This field is required!" } }}
+                      render={({ field, fieldState: { error } }) => (
+                        <FormControl variant="standard" fullWidth className=" " error={!!error}>
+
+                          <InputLabel htmlFor="nativeLanguage" id="nativeLanguage-label">Native Language</InputLabel>
+                          <Select
+                            {...field}
+                            id="nativeLanguage"
+                            aria-labelledby="nativeLanguage-label"
+                            fullWidth
+                          // variant="standard"
+                          >
+                            {languageChoices?.map(([value, label]) => (
+                              <MenuItem key={value} value={value}>{label}</MenuItem>
+                            ))}
+                          </Select>
+                          <FormHelperText>{error?.message}</FormHelperText>
+
+                        </FormControl>
                       )}
                     />
 
+
                   </div>
 
+
                   <div className="col-12 col-lg-5 py-4 pe-xl-4 max-400 mx-auto  mx-lg-0">
-                    <InputLabel htmlFor="originCountry" id="originCountry-label">Origin Country</InputLabel>
+
                     <Controller
                       name="originCountry"
                       control={control}
                       defaultValue={countryChoices[0][0]}
-                      rules={{ required: true }}
-                      render={({ field }) => (
-                        <Select
-                          {...field}
-                          id="originCountry"
-                          aria-labelledby="originCountry-label"
-                          fullWidth
-                          variant="standard"
-                        >
-                          {countryChoices?.map(([value, label]) => (
-                            <MenuItem key={value} value={value}>{label}</MenuItem>
-                          ))}
-                        </Select>
+                      rules={{ required: { value: true, message: "This field is required!" } }}
+                      render={({ field, fieldState: { error } }) => (
+                        <FormControl variant="standard" fullWidth error={!!error}>
+                          <InputLabel htmlFor="originCountry" id="originCountry-label">Origin Country</InputLabel>
+                          <Select
+                            {...field}
+                            error={!!error}
+                            id="originCountry"
+                            aria-labelledby="originCountry-label"
+                            fullWidth
+                          >
+                            {countryChoices?.map(([value, label]) => (
+                              <MenuItem key={value} value={value}>{label}</MenuItem>
+                            ))}
+                          </Select>
+                          <FormHelperText>{error?.message}</FormHelperText>
+                        </FormControl>
                       )}
                     />
                   </div>
 
                   <div className="col-12 col-lg-5 py-4 pe-xl-4 max-400 mx-auto  mx-lg-0">
-                    <InputLabel htmlFor="gender" id="gender-label">Gender</InputLabel>
                     <Controller
                       name="gender"
                       control={control}
                       defaultValue={genderChoices[0][0]}
-                      rules={{ required: true }}
-                      render={({ field }) => (
-                        <Select
-                          {...field}
-                          id="gender"
-                          aria-labelledby="gender-label"
-                          fullWidth
-                          variant="standard"
-                          className=""
-                        >
-                          {genderChoices?.map(([value, label]) => (
-                            <MenuItem key={value} value={value}>{label}</MenuItem>
-                          ))}
-                        </Select>
+                      rules={{ required: { value: true, message: "This field is required!" } }}
+                      render={({ field, fieldState: { error } }) => (
+                        <FormControl variant="standard" fullWidth error={!!error}>
+                          <InputLabel size="small" htmlFor="gender" id="gender-label">Gender</InputLabel>
+                          <Select
+                            {...field}
+                            error={!!error}
+                            id="gender"
+                            aria-labelledby="gender-label"
+                            fullWidth
+                            className=""
+                          >
+                            {genderChoices?.map(([value, label]) => (
+                              <MenuItem key={value} value={value}>{label}</MenuItem>
+                            ))}
+                          </Select>
+                        </FormControl>
                       )}
                     />
 
@@ -368,14 +356,14 @@ export default function ContactUs() {
                     name="arabicLevel"
                     control={control}
                     defaultValue={arabicLevelChoices[0][0]}
-                    rules={{ required: true }}
-                    render={({ field }) => (
+                    rules={{ required: { value: true, message: "This field is required!" } }}
+                    render={({ field, fieldState: { error } }) => (
                       <>
                         <FormLabel id="arabic-level-label" className="py-3">Arabic Level</FormLabel>
                         <RadioGroup
                           row
                           aria-labelledby="arabic-level-label"
-                          // defaultValue={arabicLevelChoices[0][0]}
+                          defaultValue={arabicLevelChoices[0][0]}
                           name="radio-buttons-group"
                           className="row d-flex justify-content-center col-12 align-items-center gap-4 gap-lg-0"
                           {...field}
@@ -411,7 +399,7 @@ export default function ContactUs() {
                 <div className="d-flex justify-content-center justify-content-lg-end col-12 py-5 pt-lg-7">
                   <MyButton
                     type="submit"
-                    isDisabled={isSubmitButtonDisabled()}
+                    isDisabled={!isDirty || submitting}
                     text={submitting ? "Submitting..." : "Send Message"}
                     className="btn text-primary bg-secondary-2 col-auto button--medium "
                   ></MyButton>
